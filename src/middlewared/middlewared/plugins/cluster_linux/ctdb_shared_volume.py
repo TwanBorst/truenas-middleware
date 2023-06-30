f974cb3cf726b70f14d610791cba4f0faa5f7741
Andrew Walker <awalker@ixsystems.com>
2023-06-30 04:01:48 -0700

    More work

--- src/middlewared/middlewared/plugins/cluster_linux/ctdb_shared_volume.py
+++ src/middlewared/middlewared/plugins/cluster_linux/ctdb_shared_volume.py
@@ -2,18 +2,22 @@ import errno
 import json
 from pathlib import Path
 from glustercli.cli import volume
+from pyglf import GLFSError
+from time import sleep
 
 from middlewared.service import Service, CallError, job
 from middlewared.plugins.cluster_linux.utils import CTDBConfig
-from middlewared.plugins.gluster_linux.utils import GlusterConfig
+from middlewared.plugins.gluster_linux.utils import GlusterConfig, get_glusterd_uuid
 
 
 MOUNT_UMOUNT_LOCK = CTDBConfig.MOUNT_UMOUNT_LOCK.value
 CRE_OR_DEL_LOCK = CTDBConfig.CRE_OR_DEL_LOCK.value
-LEGACY_CTDB_VOL_NAME = CTDBConfig.CTDB_VOL_NAME.value
+LEGACY_CTDB_VOL_NAME = CTDBConfig.LEGACY_CTDB_VOL_NAME.value
 CTDB_LOCAL_MOUNT = CTDBConfig.CTDB_LOCAL_MOUNT.value
 CTDB_VOL_INFO_FILE = CTDBConfig.CTDB_VOL_INFO_FILE.value
 METADATA_VOL_FILE = GlusterConfig.METADATA_VOLUME.value
+CTDB_STATE_DIR = CTDBConfig.CTDB_STATE_DIR.value
+MIGRATION_WAIT_TIME = 300
 
 
 class CtdbSharedVolumeService(Service):
@@ -22,14 +26,14 @@ class CtdbSharedVolumeService(Service):
         namespace = 'ctdb.shared.volume'
         private = True
 
-    def get_metadata_config(self):
+    def get_vol_and_path(self):
         try:
             with open(METADATA_VOL_FILE, 'r') as f:
                 vol_name = f.read()
 
             return {
                 'volume': vol,
-                'metadata_dir': '.truenas_metadata'
+                'metadata_dir': CTDB_STATE_DIR
             }
         except FileNotFoundError:
             pass
@@ -39,10 +43,15 @@ class CtdbSharedVolumeService(Service):
             try:
                 self.middleware.call_sync('gluster.filesystem.lookup', {
                     'volume_name': LEGACY_CTDB_VOL_NAME,
-                    'path': '.DEPRECATED' 
+                    'path': '.DEPRECATED'
                 })
-            except Exception:
-                self.logger.debug("failed to look up sentinel", exc_info=True)
+            except GLFSError as e:
+                if e.errno != errno.ENOENT:
+                    raise CallError(
+                        'Failed to lookup DEPRECATED sentinel for legacy CTDB '
+                        f'shared volume: {e.errmsg}', e.errno
+                    )
+
                 return {
                     'volume': LEGACY_CTDB_VOL_NAME,
                     'metadata_dir': '/'
@@ -55,25 +64,30 @@ class CtdbSharedVolumeService(Service):
             try:
                 self.middleware.call_sync('gluster.filesystem.lookup', {
                     'volume_name': vol,
-                    'path': '.truenas_metadata' 
+                    'path': CTDB_STATE_DIR
                 })
-            except Exception:
-                self.logger.debug("no metadata dir on %s", vol, exc_info=True) 
+            except GLFSError as e:
+                if e.errno != errno.ENOENT:
+                    raise CallError(
+                        'Failed to lookup truenas cluster state dir on '
+                        f'volume [{vol}]: {e.errmsg}', e.errno
+                    )
+
                 continue
 
             return {
                 'volume': vol,
-                'metadata_dir': '.truenas_metadata'
+                'metadata_dir': CTDB_STATE_DIR
             }
 
-        raise CallError("No metadata dir configured")
+        raise CallError("No metadata dir configured", errno.ENOENT)
 
     def generate_info(self):
-        conf = self.get_metdata_config()
+        conf = self.get_vol_and_path()
 
-        volume = conf['volume'] 
+        volume = conf['volume']
         volume_mp = Path(CTDBConfig.LOCAL_MOUNT_BASE.value, volume)
-        metadata_dir = conf['metadata_dir'] 
+        metadata_dir = conf['metadata_dir']
         glfs_uuid = self.middleware.call_sync('gluster.filesystem.lookup', {
             'volume_name': volume,
             'path': metadata_dir
@@ -98,15 +112,21 @@ class CtdbSharedVolumeService(Service):
         except (FileNotFoundError, json.JSONDecodeError):
             return self.generate_info()
 
-    def update(self, data):
-        info = self.middleware.call_sync('gluster.volume.exists_and_started', data['name'])
+    @job(lock="ctdb_system_volume_update")
+    def update(self, job, data):
+        """
+        update is performed under lock to serialize update ops
+        and grant more visibility.
+        """
+        volume, uuid = data['name'].rsplit(':', 1)
+        info = self.middleware.call_sync('gluster.volume.exists_and_started', volume)
         if not info['exists']:
             raise CallError(
-                f'{data["name"]}: volume does not exist', errno.ENOENT 
+                f'{volume}: volume does not exist', errno.ENOENT
             )
 
         if not info['started']:
-            self.middleware.call_sync('gluster.volume.start', {'name': data['name']})
+            self.middleware.call_sync('gluster.volume.start', {'name': volume})
 
         if self.middleware.call_sync('service.started', 'ctdb'):
             raise CallError(
@@ -115,31 +135,195 @@ class CtdbSharedVolumeService(Service):
             )
 
         with open(f'{METADATA_VOL_FILE}.tmp', 'w') as f:
-            vol_name = f.write(data['name'])
+            f.write(volume)
             f.flush()
             os.fsync(f.fileno())
 
         os.rename(f'{METADATA_VOL_FILE}.tmp', METADATA_VOL_FILE)
+        gluster_uuid = get_glusterd_uuid()
+
+        try:
+            self.middleware.call_sync('gluster.filesystem.unlink', {
+                'volume_name': volume,
+                'uuid': uuid,
+                'path': f'.{gluster_uuid}_UPDATE_IN_PROGRESS'
+            })
+        except Exception:
+            self.logger.warning('Failed to unlink in-progress sentinel', exc_info=True)
+
         return self.generate_info()
+
+    def __move_ctdb_vol(self, data):
+        # do not call this method directly
+        try:
+            current = self.get_vol_and_path()
+        except CallError as e:
+            if e.errno != errno.ENOENT:
+                raise
+
+            self.logger.debug("Failed to detect metadata dir.", exc_info=True)
+            current = {'volume': None}
+
+        # If we're not moving the location, just make sure our vol file
+        # is present
+        if current['volume'] == data['name']:
+            try:
+                hdl = self.middleware.call_sync('gluster.filesystem.lookup', {
+                    'volume_name': current['volume'],
+                    'path': current['metadata_dir']
+                })
+            except GLFError as e:
+                if e.errno != errno.ENOENT:
+                    raise CallError(
+                        'Failed to lookup truenas cluster state dir '
+                        f'[{current["metadata_dir"]}] on volume '
+                        f'[{current["volume"]}]: {e.errmsg}', e.errno
+                    )
+
+                hdl = self.middleware.call_sync('gluster.filesystem.mkdir', {
+                    'volume_name': current['volume'],
+                    'path': current['metadata_dir'],
+                    'options': {'mode': 0o700}
+                })
+            return hdl['uuid']
+
+        elif current['volume'] is None:
+            hdl = self.middleware.call_sync('gluster.filesystem.mkdir', {
+               'volume_name': data['name'],
+               'path': CTDB_STATE_DIR,
+               'options': {'mode': 0o700}
+            })
+            return hdl['uuid']
+
+        src_uuid = self.middleware.call_sync('gluster.filesystem.lookup', {
+            'volume_name': current['volume'],
+            'path': current['metadata_dir']
+        })['uuid']
+
+        dst_uuid = self.middleware.call_sync('gluster.filesystem.lookup', {
+            'volume_name': data['name'],
+            'path': CTDB_STATE_DIR
+        })['uuid']
+
+        move_job = self.middleware.call_sync('gluster.filesystem.copy_tree', {
+            'src_volume_name': current['volume'],
+            'src_uuid': src_uuid,
+            'dst_volume_name': data['name'],
+            'dst_uuid': dst_uuid,
+        })
+        move_job.wait_sync(raise_error=True)
+
+        rmtree_job = self.middleware.call_sync('gluster.filesystem.rmtree', {
+            'volume_name': current['volume'],
+            'path': current['metadata_dir']
+        })
+        rmtree_job.wait_sync()
+
+        if current['volume'] == LEGACY_CTDB_VOL_NAME:
+            self.middleware.call_sync('gluster.filesystem.create_file', {
+                'volume_name': current['volume'],
+                'path': '.DEPRECATED',
+                'options': {'mode': 0o700}
+            })
+
+        return dst_uuid
 
     @job(lock=CRE_OR_DEL_LOCK)
     def migrate(self, job, data):
-        /* prior to migration must stop ctdb daemon */
-        data = {'event': 'CTDB_STOP', 'name': CTDB_VOL_NAME, 'forward': True}
-        self.middleware.call_sync('gluster.localevents.send', data)
 
+        # We must stop ctdbd fist (will fault cluster)
+        if not self.middleware.call_sync('ctdb.general.healthy'):
+            raise CallError(
+                'CTDB volume configuration may only be initiated with healthy cluster'
+            )
 
+        cur_info = self.get_vol_and_path()
+        system_dir = self.middleware.call_sync('gluster.filesystem.lookup', {
+            'volume_name': cur_info['volume'],
+            'path': cur_info['metadata_dir']
+        })
+        contents = self.middleware.call_sync('gluster.filesystem.contents', {
+            'volume_name': cur_info['volume'],
+            'uuid': system_dir['uuid'],
+        })
 
-    async def validate(self):
-        filters = [('id', '=', CTDB_VOL_NAME)]
+        for file in contents:
+            if file.endswith('_UPDATE_IN_PROGRESS')
+                peer_uuid = file_name.strip('_UPDATE_IN_PROGRESS')
+                raise CallError(
+                    f'{peer_uuid[1:]}: Gluster peer currently in process '
+                    'of updating system volume path', errno.EBUSY
+                )
+
+        self.middleware.call_sync('gluster.localevents.send', {
+            'event': 'CTDB_STOP',
+            'name': data['name'],
+            'forward': True
+        })
+
+        # By the point ctdbd is stopped on all nodes
+        # The following moves our nodes file and every other
+        # ctdb / cluster-config related file to the new volume
+        # then removes the original
+        dst_uuid = self.__move_ctdb_vol(data)
+
+        for peer in self.middleware.call_sync('gluster.peer.query'):
+            try:
+                self.middleware.call_sync('gluster.filesystem.create_file', {
+                    'volume_name': data['name'],
+                    'uuid': dst_uuid,
+                    'path': f'.{peer["uuid"]}_UPDATE_IN_PROGRESS'
+                })
+            except Exception:
+                # We're already pretty heavily commited at this point
+                # and so we'll just hope for the best that things shake out
+                # okay.
+                self.logger.warning(
+                    'Failed to generate sentinel for node %s',
+                    peer['hostname'], exc_info=True
+                )
+
+        # The event kicks off backgrounded change
+        self.middleware.call_sync('gluster.localevents.send', {
+            'event': 'METADATA_VOL_UPDATE',
+            'name': f'{data["name"]}:{dst_uuid}',
+            'forward': True
+        })
+
+        # The cluster update is a backgrounded job, and may take a small
+        # amount of time to complete.
+        remaining = MIGRATION_WAIT_TIME
+        while remaining > 0:
+            contents = self.middleware.call_sync('gluster.filesystem.contents', {
+                'volume_name': data['name'],
+                'uuid': dst_uuid,
+            })
+            for file_name in contents:
+                if not file_name.endswith('_UPDATE_IN_PROGRESS'):
+                    continue
+
+                peer_uuid = file_name.strip('_UPDATE_IN_PROGRESS')
+                job.set_progress(50, f'Waiting for peer [{peer_uuid[1:]}] to finish update.')
+                sleep(10)
+                remaining -= 10
+
+            break
+
+        # This validates our nodes file and starts ctdb
+        self.middleware.call_sync('ctdb.shared.volume.create')
+
+    async def validate(self, ctdb_vol_name):
+        filters = [('id', '=', ctdb_vol_name)]
         ctdb = await self.middleware.call('gluster.volume.query', filters)
         if not ctdb:
             # it's expected that ctdb shared volume exists when
             # calling this method
-            raise CallError(f'{CTDB_VOL_NAME} does not exist', errno.ENOENT)
+            raise CallError(f'{ctdb_vol_name} does not exist', errno.ENOENT)
 
+        # This should generate alert rather than exception
+        """
         for i in ctdb:
-            err_msg = f'A volume named "{CTDB_VOL_NAME}" already exists '
+            err_msg = f'A volume named "{ctdb_vol_name}" already exists '
             if i['type'] != 'REPLICATE':
                 err_msg += (
                     'but is not a "REPLICATE" type volume. '
@@ -147,12 +331,14 @@ class CtdbSharedVolumeService(Service):
                 )
                 raise CallError(err_msg)
             elif i['replica'] < 3 or i['num_bricks'] < 3:
-                err_msg += (
+                err_msg +=(
                     'but is configured in a way that '
                     'could cause data corruption. Please delete '
                     'or rename this volume and try again.'
                 )
                 raise CallError(err_msg)
+        """
+        return
 
     @job(lock=CRE_OR_DEL_LOCK)
     async def create(self, job):
@@ -160,57 +346,29 @@ class CtdbSharedVolumeService(Service):
         Create and mount the shared volume to be used
         by ctdb daemon.
         """
-        # check if ctdb shared volume already exists and started
-        info = await self.middleware.call('gluster.volume.exists_and_started', CTDB_VOL_NAME)
-        if not info['exists']:
-            # get the peers in the TSP
-            peers = await self.middleware.call('gluster.peer.query')
-            if not peers:
-                raise CallError('No peers detected')
-
-            # shared storage volume requires 3 nodes, minimally, to
-            # prevent the dreaded split-brain
-            con_peers = [i['hostname'] for i in peers if i['connected'] == 'Connected']
-            if len(con_peers) < 3:
-                raise CallError(
-                    '3 peers must be present and connected before the ctdb '
-                    'shared volume can be created.'
-                )
-
-            # get the system dataset location
-            ctdb_sysds_path = (await self.middleware.call('systemdataset.config'))['path']
-            if not ctdb_sysds_path:
-                raise CallError("System dataset is not properly mounted")
-
-            ctdb_sysds_path = str(Path(ctdb_sysds_path).joinpath(CTDB_VOL_NAME))
-
-            bricks = []
-            for i in con_peers:
-                bricks.append(i + ':' + ctdb_sysds_path)
-
-            options = {'args': (CTDB_VOL_NAME, bricks,)}
-            options['kwargs'] = {'replica': len(con_peers), 'force': True}
-            await self.middleware.call('gluster.method.run', volume.create, options)
+        meta_config = await self.middleware.call('ctdb.shared.volume.metadata_config')
+        vol = meta_config['volume']
+        info = await self.middleware.call('gluster.volume.exists_and_started', meta_config['volume'])
 
         # make sure the shared volume is configured properly to prevent
         # possibility of split-brain/data corruption with ctdb service
-        await self.middleware.call('ctdb.shared.volume.validate')
+        await self.middleware.call('ctdb.shared.volume.validate', vol)
 
         if not info['started']:
             # start it if we get here
-            await self.middleware.call('gluster.volume.start', {'name': CTDB_VOL_NAME})
+            await self.middleware.call('gluster.volume.start', {'name': meta_config['volume']})
 
         # try to mount it locally and send a request
         # to all the other peers in the TSP to also
         # FUSE mount it
-        data = {'event': 'VOLUME_START', 'name': CTDB_VOL_NAME, 'forward': True}
+        data = {'event': 'VOLUME_START', 'name': vol, 'forward': True}
         await self.middleware.call('gluster.localevents.send', data)
 
         # we need to wait on the local FUSE mount job since
         # ctdb daemon config is dependent on it being mounted
         fuse_mount_job = await self.middleware.call('core.get_jobs', [
             ('method', '=', 'gluster.fuse.mount'),
-            ('arguments.0.name', '=', 'ctdb_shared_vol'),
+            ('arguments.0.name', '=', vol),
             ('state', '=', 'RUNNING')
         ])
         if fuse_mount_job:
@@ -255,10 +413,10 @@ class CtdbSharedVolumeService(Service):
 
         # this sends an event telling all peers in the TSP (including this system)
         # to start the ctdb service
-        data = {'event': 'CTDB_START', 'name': CTDB_VOL_NAME, 'forward': True}
+        data = {'event': 'CTDB_START', 'name': meta_config['volume'], 'forward': True}
         await self.middleware.call('gluster.localevents.send', data)
 
-        return await self.middleware.call('gluster.volume.query', [('name', '=', CTDB_VOL_NAME)])
+        return await self.middleware.call('gluster.volume.query', [('name', '=', vol)])
 
     @job(lock=CRE_OR_DEL_LOCK)
     async def teardown(self, job, force=False):
